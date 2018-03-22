544e63d24603c9d2f661370db4e24cdb4f381614
Christopher Dorst <chris@dorst.email>
2018-03-22 17:20:18 -0500

    Add TableReferenceGetter.cs

---
+++ Azure.Storage.Table.GetTableReference/TableReferenceGetter.cs
@@ -0,0 +1,12 @@
+using Microsoft.Azure;
+using static Azure.Storage.Table.GetTableClient.TableClientGetter;
+
+namespace Azure.Storage.Table.GetTableReference
+{
+    /// <summary>Function returns a reference of a Microsoft Azure CloudTable using the given connection string and table name</summary>
+    public static class TableReferenceGetter
+    {
+        /// <summary>Returns a reference of a Microsoft Azure CloudTable using the given connection string and table name</summary>
+        public static CloudTable TableReference(string connectionString, string tableName) => TableClient(connectionString).GetTableReference(tableName);
+    }
+}
